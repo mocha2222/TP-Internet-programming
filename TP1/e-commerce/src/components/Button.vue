428cc36ad0706533de57cffcf3877ef3361b6946
mocha2222 <bormeyprak@gmail.com>
2025-10-23 23:10:47 +0700

    Initial commit

---
+++ TP1/e-commerce/src/components/Button.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <button class="btn">
+    <p>Shop Now -></p>
+  </button>
+  
+</template>
+
+<style scoped>
+    .btn{
+        border-radius: 3px;
+        width: 100px;
+        height: 30px;
+        display: flex;
+        justify-content: center; 
+        align-items: center;   
+        color: white;
+        font-weight: 600;
+        border-style: none;
+        font-size: 10px;
+    }
+</style>
